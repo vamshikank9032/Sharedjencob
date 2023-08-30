b14f8a361913012b9709b1f9359b5a2edcf94453
vamshikank9032 <83363074+vamshikank9032@users.noreply.github.com>
2023-08-30 17:30:17 +0530

    Update createec2.groovy

--- vars/createec2.groovy
+++ vars/createec2.groovy
@@ -1,1 +1,47 @@
-Write groovy here
+def call(Map pipelineParams)
+
+pipeline {
+    agent any
+    parameters {
+        string(name: 'KeyName', defaultValue: 'Putty-Test')
+        choice(name: 'awsRegion', defaultValue: 'us-east-2')
+    }
+
+    environment {
+        AWS_DEFAULT_REGION = "${params.awsRegion}"
+        KeyName = "${params.KeyName}"
+         }
+    
+    stages {
+        stage('Git Checkout') {
+            steps {
+              sh "ls -lat"  
+              git branch: 'master', url: 'https://github.com/vamshikank9032/Sharedjencob.git'  
+            }
+        }
+        stage('Deploy CF Template') {
+                steps {
+                    script {
+                       //def amiId = 'ami-0ccabb5f82d4c9af5'
+                         sh 'echo "Running cfn template Validation"'
+                   sh """
+                   ls
+                   pwd
+                   cd cloudformation/
+                   ls
+                   aws cloudformation deploy --region ${AWS_DEFAULT_REGION} --template-file EC2.yaml --stack-name sample-EC2-CF1 
+                    """
+                }
+                }
+                }
+                           
+    }
+    post {
+        success {
+            echo "CloudFormation stack is deployed successfully"
+        }
+        failure {
+            echo "CloudFormation stack failed"
+        }
+    }
+}
